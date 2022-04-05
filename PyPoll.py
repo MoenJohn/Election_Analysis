831b8eb9f02c90da31589b1a905d61f14914ce3c
[John Moen] <[gionnym4@gmail.com]>
2022-04-05 18:06:10 -0500

    Fixed file upload path

--- PyPoll.py
+++ PyPoll.py
@@ -18,14 +18,18 @@ now = dt.datetime.now()
 # Print the present time.
 print("The time right now is ", now)
 
-import os
 import csv
+import os
+
+print(os.getcwd())
+
+#BASE = os.path.dirname(os.path.abspath(__file__))
+#file_to_load = os.path.join(BASE, 'Resources', 'election_results.csv')
 
 # Assign a variable for the file to load and the path.
-file_to_load = 'Resources/election_results.csv'
-
-# Open the election results and read the file
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Open the election results and read the file.
 with open(file_to_load) as election_data:
 
-     # To do: perform analysis.
+    # Print the file object.
      print(election_data)
